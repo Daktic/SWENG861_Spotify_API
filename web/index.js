c69c30b9475a4af5e28318b39e3b5e980da09e3d
Daktic <gberg@DakticDigital.com>
2023-06-11 14:18:34 -0400

    adjusted the response to spawn artist tiles for each response.

--- web/index.js
+++ web/index.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function (event) {
+document.addEventListener("DOMContentLoaded", function () {
     const button = document.getElementById("submit-btn");
     const radio = document.getElementById("query-type");
     const textInput = document.getElementById("text-input")
@@ -25,6 +25,31 @@ document.addEventListener("DOMContentLoaded", function (event) {
             .then(data => {
                 // Process the data returned from the API
                 console.log(data);
+                // Get the container element
+                const container = document.getElementById('artists-container');
+
+                // Iterate over each artist object
+                data.artists.forEach(artist => {
+                    console.log(artist)
+                    // Create elements to display artist information
+                    const artistElement = document.createElement('div');
+                    const nameElement = document.createElement('h2');
+                    const followersElement = document.createElement('p');
+                    const imageElement = document.createElement('img');
+
+                    // Set the content of the elements
+                    nameElement.textContent = artist.name;
+                    followersElement.textContent = `followers: ${artist.followers.total}`;
+
+                    const imageUrl = artist.images[1] ? artist.images[1].url : 'https://www.freepnglogos.com/images/spotify-logo-png-7053.html';
+                    imageElement.setAttribute('src', imageUrl)
+
+                    // Append the elements to the artist container
+                    artistElement.appendChild(nameElement);
+                    artistElement.appendChild(followersElement);
+                    artistElement.appendChild(imageElement);
+                    container.appendChild(artistElement);
+                });
             })
             .catch(error => {
                 // Handle any errors that occurred during the request
