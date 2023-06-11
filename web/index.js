e6828e82fc1a127c9cbaa4cdc6ab0b17b2cf9c86
Daktic <gberg@DakticDigital.com>
2023-06-11 15:41:16 -0400

    Changed styling

--- web/index.js
+++ web/index.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     radioInputs.forEach((radioInput) => {
         radioInput.addEventListener("click", function () {
-            radio.value = `Search ${radioInput.value}`;
+            radio.innerText = `Search ${radioInput.value}`;
         })
     });
 
@@ -28,21 +28,33 @@ document.addEventListener("DOMContentLoaded", function () {
                 data.artists.forEach(artist => {
                     // Create elements to display artist information
                     const artistElement = document.createElement('div');
+                    const artistStats = document.createElement('div');
+                    const artistItems = document.createElement('div');
                     const nameElement = document.createElement('h2');
                     const followersElement = document.createElement('p');
                     const imageElement = document.createElement('img');
 
                     // Set the content of the elements
                     nameElement.textContent = artist.name;
+                    nameElement.setAttribute('class', 'artist-name');
                     followersElement.textContent = `followers: ${artist.followers.total}`;
+                    followersElement.setAttribute('class', 'artist-followers');
 
                     const imageUrl = artist.images[1] ? artist.images[1].url : 'https://www.freepnglogos.com/images/spotify-logo-png-7053.html';
                     imageElement.setAttribute('src', imageUrl)
+                    imageElement.setAttribute('class', 'artist-image')
+
+                    artistElement.setAttribute('class', 'artist-card')
+                    artistItems.setAttribute('class', 'artist-items')
+                    artistStats.setAttribute('class', 'artist-stats')
 
                     // Append the elements to the artist container
-                    artistElement.appendChild(nameElement);
-                    artistElement.appendChild(followersElement);
-                    artistElement.appendChild(imageElement);
+                    artistStats.appendChild(followersElement);
+                    artistItems.appendChild(nameElement);
+                    artistItems.appendChild(imageElement);
+                    artistElement.appendChild(artistItems);
+                    artistElement.appendChild(artistStats);
+
                     container.appendChild(artistElement);
                 });
             })
